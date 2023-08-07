9e6ec56bdd7adab62fd2efc3225216a227c830c8
Sergio <sergiomolinaflores1@gmail.com>
2023-08-07 19:54:19 -0400

    Dependencias y permiso de internet Ok

---
+++ app/src/main/java/cl/samf/invidivual8_mod6/MainActivity.kt
@@ -0,0 +1,28 @@
+package cl.samf.invidivual8_mod6
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+/*
+[] 2 pantallas: listado y detalle
+[] Permiso Internet
+[] dependencias: viewBinding, coil, room, retrofit, navigation, viewmodel
+[] Listado:
+    [] RecyclerView + ViewHolder + Adapter
+[] Detalle:
+    [] Recyclerview + ViewHolder + Adapter
+[] repositorio
+[] Remoto
+    [] dataClass
+    [] API (Interface)
+    [] RetroFit
+[] Local
+    [] Database
+    [] Entity
+    [] DAO
+*/
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
